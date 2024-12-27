f7ae6aa935615e78c36d817817817cf41760d465
Davi Silva <davisilvaphoto@gmail.com>
2024-12-27 18:20:59 -0300

    adjust type

--- src/di/Injectable.ts
+++ src/di/Injectable.ts
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Constructor } from '../types/utils';
 import { Registry } from './Registry';
 
-export const Injectable = (token: string) => (target: Constructor<any>) =>
+export const Injectable = (token: string) => (target: Constructor<unknown>) =>
   Registry.getInstance().register(token, target);
